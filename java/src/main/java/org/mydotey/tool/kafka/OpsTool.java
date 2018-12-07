d8af14950d9916b67cd510332d3f387ce45fb7d3
Qiang Zhao <koqizhao@outlook.com>
2018-12-07 15:51:42 +0800

    init commit

---
+++ java/src/main/java/org/mydotey/tool/kafka/OpsTool.java
@@ -0,0 +1,9 @@
+package org.mydotey.tool.kafka;
+
+public class OpsTool {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ java/src/test/java/org/mydotey/tool/kafka/OpsToolTest.java
@@ -0,0 +1,10 @@
+package org.mydotey.tool.kafka;
+
+/**
+ * @author koqizhao
+ *
+ * Dec 4, 2018
+ */
+public class OpsToolTest {
+
+}
