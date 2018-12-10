5e23ad5bb5f0616a12add95c9d12f3dd61436376
Qiang Zhao <koqizhao@outlook.com>
2018-12-10 19:16:41 +0800

    add scripts: broker-data-transfer.sh/reassignment-status-printer.sh

--- java/src/main/java/org/mydotey/tool/kafka/ops/Tools.java
+++ java/src/main/java/org/mydotey/tool/kafka/ops/Tools.java
@@ -1,6 +1,11 @@
 package org.mydotey.tool.kafka.ops;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+
+import net.sourceforge.argparse4j.helper.HelpScreenException;
 
 public class Tools {
 
@@ -11,11 +16,32 @@ public class Tools {
         _tools.put(ReassignmentStatusPrinter.class, "show reassignment status");
     }
 
-    public static void main(String[] args) {
-        System.out.println("kafka ops tool list\n");
-        _tools.forEach((t, d) -> {
-            System.out.printf("class: %s\ndetails:\n%s\n\n", t.getName(), d);
-        });
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("kafka ops tool list\n");
+            _tools.forEach((t, d) -> {
+                System.out.printf("class: %s\ndetails:\n%s\n\n", t.getName(), d);
+            });
+
+            return;
+        }
+
+        Class<?> clazz = Class.forName(args[0]);
+        if (!_tools.containsKey(clazz)) {
+            System.err.println("unknow tool: " + clazz);
+            return;
+        }
+
+        Method mainMethod = clazz.getMethod("main", String[].class);
+        try {
+            mainMethod.invoke(null, new Object[] { Arrays.copyOfRange(args, 1, args.length) });
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof HelpScreenException)
+                return;
+
+            cause.printStackTrace();
+        }
     }
 
 }
---
+++ scripts/broker-data-transfer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar lib/kafka-ops.jar org.mydotey.tool.kafka.ops.BrokerDataTransfer $@
+
